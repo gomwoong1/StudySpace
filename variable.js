000fcd0ce04384bb22fc3b157e2ea2737073dcfa
gomwoong1 <gomwoong0619@naver.com>
2022-07-01 17:22:13 +0900

    0701

---
+++ variable.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('Hello World!');
+
+let char = 'hello';
+let char2 = "world";
+
+console.log(char);
+
+let good = char / 2;
+
+console.log(good);
+
+console.log(`good`);
