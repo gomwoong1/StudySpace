7692f0aa68f9fb1f057067a4a6d66f8768a0d261
gomwoong1 <gomwoong0619@naver.com>
2022-07-06 23:27:49 +0900

    0706
    연습

--- 0706/server.js
+++ 0706/server.js
@@ -1,6 +1,13 @@
+//npm install npm, express, ejs
+
 const express = require('express');
 const app =  express();
 const port = 3001;
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(port, () => {
     console.log(`Server Open: ${port} port`);
