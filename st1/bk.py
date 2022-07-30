90912653fd06e910b7a3b6263dd8aa4d5a532605
gomwoong1 <gomwoong0619@naver.com>
2022-07-31 00:04:23 +0900

    0731
    파이썬 공부

--- st1/bk.py
+++ st1/bk.py
@@ -28,3 +28,24 @@
 # a, b = input().split()
 # print(int(a)-int(b))
 
+# 첫째 줄에 A×B를 출력한다.
+# a, b = map(int, input().split())
+# print(a*b)
+
+# 첫째 줄에 A/B를 출력한다
+# a, b = map(int, input().split())
+# print(a/b)
+
+# 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+# a, b = map(int, input().split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+
+# 첫째 줄에 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어진다. 
+# 첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
+# id = str(input())
+# print(id+'??!')
+
