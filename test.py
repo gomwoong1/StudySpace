368bd1f66532319d9c492c4b347bf15adcb48f66
gomwoong1 <gomwoong0619@naver.com>
2022-12-09 10:51:24 +0900

    1209
    - 처커파 수업

--- test.py
+++ test.py
@@ -12,3 +12,37 @@
 
 # 2
 
+# a = int(input())
+
+# def is_even(a):
+#     if a % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# if(is_even(a)):
+#     print("짝수입니다.")
+# else:
+#     print("홀수입니다.")
+
+#-----------------------------------------------------
+
+# 3
+
+# price = int(input())
+
+# def cal(price):
+
+#     # tip = price * 0.15
+#     # tax = price * 0.095
+#     # return price + tip + tax
+#     return price + (price * 0.095) + (price * 0.15)
+
+# total = cal(price)
+
+# print("총 가격은", total, "원 입니다.")
+
+#-----------------------------------------------------
+
+# 4
+
