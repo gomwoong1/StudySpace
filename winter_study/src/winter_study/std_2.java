c5ea7cb3227b29c2b2b2d1f5d3d0e826acff3aba
gomwoong1 <gomwoong0619@naver.com>
2022-12-28 16:24:24 +0900

    1228 - 스터디 공부

--- winter_study/src/winter_study/std_2.java
+++ winter_study/src/winter_study/std_2.java
@@ -1,31 +1,44 @@
 package winter_study;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class std_2 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int val = sc.nextInt(); //반복 횟수 입력받기
-		sc.nextLine(); //개행문자 제거
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//Scanner sc = new Scanner(System.in) 대체
+		int val = Integer.parseInt(br.readLine());
+		String[] result = new String[val]; //결과를 저장할 문자열 배열
+		Stack<Character> stack = new Stack<>();
 		
-		for(int i=0; val > i; i++) {
-			String cmd = sc.nextLine();
+		for(int i=0; i < val; i++) {
+			stack.clear();
+			String cmd = br.readLine();
+			for(int j = 0; j < cmd.length(); j++) {
+				if(cmd.charAt(j) == ')') {
+					if(stack.size() == 0) {
+						stack.push(cmd.charAt(j));
+						break;
+					}
+					else
+						stack.pop();
+				}
+				else if(cmd.charAt(j) == '(') {
+					stack.push(cmd.charAt(j));
+				}
+			}
 			
-			System.out.println(cntChar(cmd, 'a'));
+			if(stack.size() == 0)
+				result[i] = "YES";
+			else
+				result[i] = "NO";
 		}
 		
-		sc.close();
-	}
-	
-	public static int cntChar(String cmd, char target) {
-		int cnt = 0;
-		
-		for(int i=0; i < cmd.length(); i++) {
-			if(cmd.charAt(i) == target)
-				cnt++;
-		}
-		return cnt;
+		for(String i : result)
+		System.out.println(i);
+		br.close();
 	}
 }
-
