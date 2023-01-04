c73f295e0610b7abc5e2983edbc590428fdc0af4
gomwoong1 <gomwoong0619@naver.com>
2023-01-04 14:53:02 +0900

    0104 스터디2 과제

--- winter_study/src/winter_study/std_3.java
+++ winter_study/src/winter_study/std_3.java
@@ -7,25 +7,24 @@ import java.io.InputStreamReader;
 public class std_3 {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int val = Integer.parseInt(bf.readLine());
-		String[] temp = bf.readLine().split(" ");
-		int max = -999;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int[] value = new int[val];
+		int cnt = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < val; i++) {
-			value[i] = Integer.parseInt(temp[i]);
+		String str = br.readLine();
+		String str2 = br.readLine();
+		String str3 = br.readLine();
+		String result = "";
+		
+		for(int i = 0; i < str.length(); i++) {
+			char a = str.charAt(i), b = str2.charAt(i), c = str3.charAt(i);
+			if (a == b && a == c)
+				result += a;
+			else
+				result += '?';
 		}
 		
-		for(int i = 0; i < val; i++) {
-			for(int j = i+1; j < val; j++) {
-				int sum = value[i]+value[j];
-				System.out.println(value[i] + " + " + value[j] + " = " + sum);
-				if( value[i]+value[j] > max )
-					max = value[i]+value[j];
-			}
-		}
-		System.out.println(max);
+		System.out.println(result);
 	}
+
 }
