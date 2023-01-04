f928ea76073678d532fd3a79d8e87126d725d04c
gomwoong1 <gomwoong0619@naver.com>
2023-01-04 14:03:49 +0900

    0104 스터디2 과제

--- winter_study/src/winter_study/std_3.java
+++ winter_study/src/winter_study/std_3.java
@@ -10,7 +10,22 @@ public class std_3 {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int val = Integer.parseInt(bf.readLine());
 		String[] temp = bf.readLine().split(" ");
-
+		int max = -999;
+		
+		int[] value = new int[val];
+		
+		for(int i = 0; i < val; i++) {
+			value[i] = Integer.parseInt(temp[i]);
+		}
+		
+		for(int i = 0; i < val; i++) {
+			for(int j = i+1; j < val; j++) {
+				int sum = value[i]+value[j];
+				System.out.println(value[i] + " + " + value[j] + " = " + sum);
+				if( value[i]+value[j] > max )
+					max = value[i]+value[j];
+			}
+		}
+		System.out.println(max);
 	}
-
 }
