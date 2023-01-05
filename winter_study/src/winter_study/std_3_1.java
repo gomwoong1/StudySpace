783b80e468df0128749683ffa5fdf39c7a28fad5
gomwoong1 <gomwoong0619@naver.com>
2023-01-05 09:50:07 +0900

    0105 스터디2 과제

--- winter_study/src/winter_study/std_3_1.java
+++ winter_study/src/winter_study/std_3_1.java
@@ -10,7 +10,7 @@ public class std_3_1 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] str = br.readLine().toUpperCase().toCharArray();
 		int count = 0, max = 0;
-		char result;
+		char result = '?';
 		
 		for(int i = 65; i <= 91; i++) {
 			count = 0;
@@ -18,14 +18,16 @@ public class std_3_1 {
 				if (i == j) {
 					count += 1;
 					if (count > max) {
-						result = 
+						result = (char) i;
+						max = count;
 					}
+					else if (count == max)
+						result = '?';
 				}
-					
 			}
 		}
 		
-		System.out.println();
+		System.out.println(result);
 	}
 
 }
