1b144a67ae218ce15aa9e2ca25d6ca046d131655
gomwoong1 <gomwoong0619@naver.com>
2023-01-12 10:54:55 +0900

    0112 자바 과제

--- winter_study/src/winter_study/std_4.java
+++ winter_study/src/winter_study/std_4.java
@@ -11,26 +11,24 @@ public class std_4 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int sum = 0;
-		String nextVal = "", origin = br.readLine(), val;
+		String nextVal = "", origin = br.readLine(), val = "";
 		
 		if( origin.length() == 1 ) 
 			val = "0" + origin;
 		else
 			val = origin;
 		
-		System.out.println("길이: " + val.length() + ", 값: " + val);
-		System.out.println(val.charAt(0) + ", " + val.charAt(1));
-		
 		while(true) {
+			if ( val.length() == 1)
+				val = "0" + origin;
+			
 			sum = Character.getNumericValue(val.charAt(0)) + Character.getNumericValue(val.charAt(1));
-//			val = (val.charAt(1) + String.valueOf(sum).substring(1));
-			val = (val.charAt(1) + String.valueOf(sum).substring(0, (int)((Math.log10(sum)+1)-1)));
+			nextVal = (val.charAt(1) + String.valueOf(sum).substring(1));
+			val = nextVal;
 			count++;
 			
-			if(val.equals(nextVal))
+			if(val.equals(origin))
 				break;
-//			System.out.println("val값: " + val.charAt(1) + ", sum값: " + String.valueOf(sum) + "/... " +val);
-//			System.out.println("origin: " + origin + ", val값: " + nextVal + ", sum값: " + sum + ", count: " + count);
 		}
 		
 		System.out.println(count);
