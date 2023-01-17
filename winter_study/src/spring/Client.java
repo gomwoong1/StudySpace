5ed6d4aa34bd47ff9617c9aa17e5124dd454468a
gomwoong1 <gomwoong0619@naver.com>
2023-01-17 18:52:37 +0900

    0117 자바 스터디

--- winter_study/src/spring/Client.java
+++ winter_study/src/spring/Client.java
@@ -3,7 +3,14 @@ package spring;
 public class Client {
 
 	public static void main(String[] args) {
+		Animal baduck = new Dog();
+		Animal nabi = new Cat();
 		
-	}
+		baduck.playWithOwner();
 
+		System.out.println();
+		System.out.println();
+		
+		nabi.playWithOwner();
+	}
 }
