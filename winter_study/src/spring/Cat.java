de179adc984571d514795b90d8539aa70495ed52
gomwoong1 <gomwoong0619@naver.com>
2023-01-17 18:50:49 +0900

    0117 자바 스터디

--- winter_study/src/spring/Cat.java
+++ winter_study/src/spring/Cat.java
@@ -2,4 +2,9 @@ package spring;
 
 public class Cat extends Animal {
 
+	@Override
+	void play() {
+		System.out.println("야옹야옹");
+	}
+
 }
--- winter_study/src/spring/Dog.java
+++ winter_study/src/spring/Dog.java
@@ -2,4 +2,14 @@ package spring;
 
 public class Dog extends Animal {
 
+	@Override
+	void play() {
+		System.out.println("멍멍");
+	}
+	
+	@Override
+	void runSomething() {
+		System.out.println("훅 메서드 오버라이딩");
+	}
+
 }
