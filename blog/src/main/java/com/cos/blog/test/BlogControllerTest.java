6880f17a544cea8f66e0f8dee4f0ce2c30e6db08
gomwoong1 <gomwoong0619@naver.com>
2023-01-20 09:07:40 +0900

    0120 스프링부트 공부

---
+++ blog/src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,16 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 스프링 실행시, 기준 패키지 이하의 모든 것들을 스캔해서 메모리에 로드(new)하는 게 아님
+// 특정 어노테이션 (@)이 붙어있는 클래스 파일들을 로드(new) 해서 스프링 컨테이너에서 관리
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>Hello Springboot</h1>";
+	}
+}
