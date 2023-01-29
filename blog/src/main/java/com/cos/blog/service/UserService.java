f48b00b337ee05214056caf16d9e28662bb52662
gomwoong1 <gomwoong0619@naver.com>
2023-01-29 23:33:47 +0900

    0129 스프링부트 공부

---
+++ blog/src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,37 @@
+package com.cos.blog.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+
+/*
+ 	MEMO
+ 	
+ 	@Service
+ 	스프링이 컴포넌트 스캔을 통해 자동으로 Bean에 등록해줌. (IoC)
+ 	
+ 	
+ */
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public int 회원가입(User user) {
+		try {
+			userRepository.save(user);
+			return 1;
+		} catch (Exception e) {
+			e.getStackTrace();
+			System.out.println("UserSerivce: 회원가입() : " + e.getMessage());
+		}
+		return -1;
+	}
+}
