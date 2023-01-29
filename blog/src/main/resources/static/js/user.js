19864ab95445c6f203f8049f063b2fae7a8a0ee7
gomwoong1 <gomwoong0619@naver.com>
2023-01-29 22:53:49 +0900

    0129 스프링부트 공부

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -16,14 +16,14 @@ let index = {
 		// ajax는 비동기 호출이기 때문에 ajax 하단의 코드를 실행하다가 done 혹은 fail 수행
 		$.ajax({
 			type: "POST",
-			url: "/api/user",
+			url: "/blog/api/user",
 			data: JSON.stringify(data),   // json으로 파싱했기 때문에 BODY에 삽입 후 전송
 			contentType: "application/json; charset=utf-8",   // BODY 데이터가 어떤 타입인지 기술(MIME 타입)
-			dataType: "json"   // 서버에게 요청 후 응답이 왔을 때, 
-		}).done(function() {
+			dataType: "json"   // 서버에게 요청 후 응답이 왔을 때, 반환된 데이터의 형식을 json으로 지정함.
+		}).done(function(resp) {
 			alert("회원가입이 완료되었습니다.");
-			location.herf="/blog";
-		}).fail(function() {
+			location.href="/blog";
+		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});  
 	}
