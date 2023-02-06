36b138124805ab439af858bf0931dc3fabd1c0d8
gomwoong1 <gomwoong0619@naver.com>
2023-02-06 17:16:08 +0900

    0206 스프링부트 공부

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -29,9 +29,13 @@ let index = {
 			contentType: "application/json; charset=utf-8",   // BODY 데이터가 어떤 타입인지 기술(MIME 타입)
 			dataType: "json"   // 서버에게 요청 후 응답이 왔을 때, 반환된 데이터의 형식을 json으로 지정함.
 		}).done(function(resp) {
-			alert("회원가입이 완료되었습니다.");
+			if (resp.status == 500){
+				alert("회원가입에 실패했습니다.");
+			} else {
+				alert("회원가입이 완료되었습니다.");
+				location.href="/";
+			}
 			//console.log(resp);
-			location.href="/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});  
