798adf4f0a5535e99bc8b256e04fbf3a62664591
gomwoong1 <gomwoong0619@naver.com>
2023-02-07 20:28:38 +0900

    new project

---
+++ friendsRemake/src/main/java/com/friends/friendsremake/FriendsRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.friends.friendsremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsRemakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsRemakeApplication.class, args);
+    }
+
+}
