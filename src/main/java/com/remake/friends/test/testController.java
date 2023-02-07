585a809cd9a8a9ab286e0199da694622277dce7d
gomwoong1 <gomwoong0619@naver.com>
2023-02-08 01:00:08 +0900

    설정파일 변경

---
+++ src/main/java/com/remake/friends/test/testController.java
@@ -0,0 +1,12 @@
+package com.remake.friends.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping("/test")
+    public String test() {
+        return "<h1>Test page</h1>";
+    }
+}
