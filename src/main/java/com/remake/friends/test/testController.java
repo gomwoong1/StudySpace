dbf0cd1e29fe1c5aab9afef7d59caf1ed7e4b61b
gomwoong1 <gomwoong0619@naver.com>
2023-02-09 00:53:14 +0900

    timeleaf 연습

--- src/main/java/com/remake/friends/test/testController.java
+++ src/main/java/com/remake/friends/test/testController.java
@@ -10,8 +10,10 @@ public class testController {
     public String test(Model model) {
         int val = 3;
         int val2 = 4;
+        String str = "<h1>hello</h1>";
         model.addAttribute("value", val);
         model.addAttribute("value2", val2);
+        model.addAttribute("str", str);
         return "index";
     }
 }
