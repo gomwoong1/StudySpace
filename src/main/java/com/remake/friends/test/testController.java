cd939c22b67ee8b22cddafeb0ae3d18091b6a5e9
gomwoong1 <gomwoong0619@naver.com>
2023-02-08 16:43:09 +0900

    timeleaf 연습

--- src/main/java/com/remake/friends/test/testController.java
+++ src/main/java/com/remake/friends/test/testController.java
@@ -1,12 +1,17 @@
 package com.remake.friends.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testController {
     @GetMapping("/test")
-    public String test() {
-        return "<h1>Test Page</h1>";
+    public String test(Model model) {
+        int val = 3;
+        int val2 = 4;
+        model.addAttribute("value", val);
+        model.addAttribute("value2", val2);
+        return "index";
     }
 }
