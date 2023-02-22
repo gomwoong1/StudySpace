c978e5be29bab743b198119c499ee765e0343207
gomwoong1 <gomwoong0619@naver.com>
2023-02-22 14:25:53 +0900

    로그인 및 회원가입 함수 구현중

---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,24 @@
+let index = {
+    init: function() {
+        $('#btn_join').on("click", () => {
+            this.join();
+        });
+
+        $('#btn_login').on("click", () => {
+            this.login();
+        });
+    },
+
+    join: function() {
+        let data = {
+
+        }
+
+    },
+
+    login: function() {
+
+    }
+}
+
+index.init();
