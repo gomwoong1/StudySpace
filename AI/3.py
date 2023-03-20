f181c7ee1e413eb82ecca7800bb4a84a0d69aa05
gomwoong1 <gomwoong0619@naver.com>
2023-03-20 10:49:09 +0900

    AI
    KNN 복습

--- AI/3.py
+++ AI/3.py
@@ -1,12 +1,18 @@
 # 빙어와 도미 분류하기
 
 from sklearn.neighbors import KNeighborsClassifier
-
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-fish = pd.read_csv("/data/Fish.csv")
-print(fish)
+df = pd.read_csv(r"C:\Users\gomwoong\Desktop\git\python_practice\AI\data\Fish.csv")
+df = df.loc[(df['Species'] == 'Bream') | (df['Species'] == 'Smelt')]
+df = df[['Weight', 'Length2']]
 
-# kn = KNeighborsClassifier()
+fish_data = df.to_numpy()
+fish_data = np.round(fish_data, 2)
+fish_target = [1] * 35 + [0] * 14
 
-# kn.fit(fish_data, fish_target)
+kn = KNeighborsClassifier()
+kn.fit(fish_data, fish_target)
+print(kn.score(fish_data, fish_target))
