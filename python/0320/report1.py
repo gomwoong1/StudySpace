8cce4d591ff03ac8de69eb583217a58bd2fe6775
gomwoong1 <gomwoong0619@naver.com>
2023-03-20 15:21:53 +0900

    0320
    파이선프로그래밍 과제 저장

--- python/0320/report1.py
+++ python/0320/report1.py
@@ -1,10 +1,21 @@
-p_jul = '-' * 30
+## 자기소개 및 변수 사용 출력프로그램 ##
+
+p_jul = '-' * 32
 p_int = "변수사용 자기 소개 출력 프로그램"
-p_num2 = "인하공업전문대학 컴퓨터시스템과 3학년 A반 김지웅입니다."
-p_sapce = " "
-pp_pro = "곧 전문가가 되어 있을 것입니다."
+p_irum = "인하공전 컴시과 3학년 A반 김지웅 입니다."
+p_space = ""
+p_pro = "곧 전문가가 되어 있을 것입니다."
 
 print(p_jul)
 print(p_int)
-print(p_int)
 print(p_jul)
+print(p_space)
+print("안녕하세요.")
+print(p_irum)
+print(p_space)
+print("오늘 처음으로 파이썬을 배우는데")
+print("초보자답지 않게 재미 있습니다.")
+print(p_space)
+print("차근차근 따라가면")
+print(p_pro)
+print(p_space)
