9da52128ed474e21334e63d220c6c4a7a67749a8
gomwoong1 <gomwoong0619@naver.com>
2023-03-22 10:25:22 +0900

    0322
    AI 예습

---
+++ AI/7.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
+df = pd.read_csv(r"C:\Users\gomwoong\Desktop\git\python_practice\AI\data\Fish.csv")
+df = df.loc[(df['Species'] == 'Perch')]
+df = df[['Length2', 'Weight']]
+
+perch_length = df['Length2'].to_numpy()
+perch_weight = df['Weight'].to_numpy()
+
+train_input, test_input, train_target, test_target = train_test_split(
+    perch_length, perch_weight, random_state=42)
+
+train_input = train_input.reshape(-1, 1)
+test_input = test_input.reshape(-1, 1)
+
+knr = KNeighborsRegressor()
+knr.n_neighbors = 3
+
+knr.fit(train_input, train_target)
+print(knr.score(test_input, test_target))
+
+distance, indexes = knr.kneighbors([[50]])
+
+plt.scatter(train_input, train_target)
+plt.scatter(train_input[indexes], train_target[indexes], marker='D')
+
+plt.scatter(50, 1033, marker='^')
+plt.show()
