de6d23c9ed800a1485f0f45c10e8d4ab721c12f4
gomwoong1 <gomwoong0619@naver.com>
2023-03-26 03:05:03 +0900

    0326
    AI 예습

--- AI/8.py
+++ AI/8.py
@@ -1,5 +1,21 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+
+df = pd.read_csv('AI\data\perch_full.csv')
+perch_full = df.to_numpy()
+
+print(perch_full)
+
+# 다항 회귀 객체 생성
+# degree = 2는 기본값으로, 다항식의 차수를 설정
+# 기본값이 거듭제곱임
+poly = PolynomialFeatures()
+
+poly.fit([[2,3]])
+
+print(poly.transform([[2, 3]]))
+
+poly = PolynomialFeatures(include_bias=False)
 
