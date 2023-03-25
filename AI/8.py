a6f3b0e58747f7d19b875c5d7d5c238272744a07
gomwoong1 <gomwoong0619@naver.com>
2023-03-26 03:22:10 +0900

    0326
    AI 예습

--- AI/8.py
+++ AI/8.py
@@ -2,20 +2,36 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('AI\data\perch_full.csv')
 perch_full = df.to_numpy()
 
-print(perch_full)
+perch_weight = np.array(
+    [5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0, 
+    110.0, 115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0, 
+    130.0, 150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0, 
+    197.0, 218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0, 
+    514.0, 556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0, 
+    820.0, 850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0, 
+    1000.0, 1000.0]
+    )
 
-# 다항 회귀 객체 생성
-# degree = 2는 기본값으로, 다항식의 차수를 설정
-# 기본값이 거듭제곱임
+train_input, test_input, train_target, test_target = train_test_split(
+    perch_full, perch_weight, random_state=42)
+
 poly = PolynomialFeatures()
 
 poly.fit([[2,3]])
-
 print(poly.transform([[2, 3]]))
 
 poly = PolynomialFeatures(include_bias=False)
 
+poly.fit(train_input)
+train_poly = poly.transform(train_input)
+
+print(train_poly.shape)
+
+print(poly.get_feature_names_out())
+
+test_poly = poly.transform(test_input)
