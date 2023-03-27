90b8d3a09a80a29276087340ca135dce948c0285
gomwoong1 <gomwoong0619@naver.com>
2023-03-27 17:16:49 +0900

    0327
    파이썬 공부

---
+++ python/0327_2.py
@@ -0,0 +1,24 @@
+##연산자의 우선순위와 종류2##
+a = 19
+b = 8
+print("a//b의 결과값", a//b)
+print("a%b의 결과값", a%b)
+
+a += 8
+print("a += 8 의 결과값 :", a)
+
+a += 8
+print("a += 8 의 결과값 :", a)
+
+a //= 8
+print("a //= 8 의 결과값 :", a)
+
+a = 19
+b = 8
+c = 8
+print("a==b의 결과 값 :", a==b)
+print("a!=b의 결과 값 :", a!=b)
+print("(b==c) and (a>b)의 결과값 :", (b==c) and (a>b))
+print("(b&c)의 결과값:", (b&c))
+print("(a>>b)의 결과값:", (a>>b))
+print("(a<<b)의 결과값:", (a<<b))
