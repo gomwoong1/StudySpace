f14d4a785f44c46bc39d9bcf48a68e6f6cd123d5
gomwoong1 <gomwoong0619@naver.com>
2023-03-29 16:27:01 +0900

    0329
    Rep1. predict, fit Done

--- AI/report/rep_1.py
+++ AI/report/rep_1.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_squared_error
 class SimpleLinearRegression():
 
     # 기울기, 예측값, 저장저장할 변수 선언 및 초기화
-    coef_, intercept_, y_pred = 0.0, 0.0, 0.0
+    coef_, intercept_, y_pred = None, None, None
 
     # 선형회귀 모델 생성 및 x, y 데이터 학습
     slr = LinearRegression()
@@ -44,11 +44,19 @@ class SimpleLinearRegression():
                 mse = mean_squared_error(y_data, y_pred)
                 print("epoch=%d, 기울기=%0.4f, 절편=%0.4f, MSE: %0.2f" % (i, a, b, mse))
 
-            # 경사하강법을 통해 찾은 기울기, 절변, 예측값을 저장
-            self.coef_ = a
-            self.intercept_ = b
-            self.y_pred = a * x_data + b
-            
+        # 경사하강법을 통해 찾은 기울기, 절변, 예측값을 저장
+        self.coef_ = np.round(a,2)
+        self.intercept_ = float(np.round(b,2))
+        self.y_pred = a * x_data + b
+
+    def predict(self, X):
+        pred = np.dot(X, self.coef_) + self.intercept_
+        return pred
+
+    def score(self):
+        score = 0
+
+        return score
 
 
 # # 산점도 그래프를 통해 x, y 데이터를 시각화
@@ -64,6 +72,7 @@ class SimpleLinearRegression():
 x_data = np.array([[1], [2], [3], [4]])
 y_data = np.array([2, 5, 7, 9])
 
-lr = SimpleLinearRegression()
-lr.fit(x_data, y_data)
-print(lr.coef_, lr.intercept_, lr.y_pred)
+slr = SimpleLinearRegression()
+slr.fit(x_data, y_data)
+print(slr.coef_, slr.intercept_)
+print(slr.predict([[5]]))
