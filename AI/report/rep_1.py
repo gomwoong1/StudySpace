fa79ba73bf7db5547ea41351ba01743384e89376
gomwoong1 <gomwoong0619@naver.com>
2023-03-29 16:39:53 +0900

    0329
    AI Rep1. 소수점 정확도 이슈만 해결할 것

--- AI/report/rep_1.py
+++ AI/report/rep_1.py
@@ -1,17 +1,14 @@
 # 외부 라이브러리 import
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+# 클래스 정의
 class SimpleLinearRegression():
 
-    # 기울기, 예측값, 저장저장할 변수 선언 및 초기화
+    # 기울기, 예측값, 저장저장할 변수 선언
     coef_, intercept_, y_pred = None, None, None
 
-    # 선형회귀 모델 생성 및 x, y 데이터 학습
-    slr = LinearRegression()
-    
     def fit(self, x_data, y_data):
         # 경사하강법을 위한 초기값 설정
         # 기울기와 절편은 각각 1.0, 학습율은 0.01, 반복회수는 2000번으로 지정
@@ -44,29 +41,22 @@ class SimpleLinearRegression():
                 mse = mean_squared_error(y_data, y_pred)
                 print("epoch=%d, 기울기=%0.4f, 절편=%0.4f, MSE: %0.2f" % (i, a, b, mse))
 
-        # 경사하강법을 통해 찾은 기울기, 절변, 예측값을 저장
-        self.coef_ = np.round(a,2)
-        self.intercept_ = float(np.round(b,2))
+        # 경사하강법 종료 후 기울기, 절편, 예측값을 각 인스턴스 변수에 저장
+        self.coef_ = a
+        self.intercept_ = float(b)
         self.y_pred = a * x_data + b
 
     def predict(self, X):
         pred = np.dot(X, self.coef_) + self.intercept_
         return pred
 
-    def score(self):
-        score = 0
-
-        return score
-
-
-# # 산점도 그래프를 통해 x, y 데이터를 시각화
-# plt.scatter(x_data, y_data)
-
-# # 모델이 찾은 기울기와 절편을 시각화
-# # 경사하강법을 통해 찾은 기울기와 절편을 시각화
-# plt.plot(x_data, slr_a * x_data + slr_b, color='black')
-# plt.plot(x_data, y_pred, color='yellow', linestyle='--')
-# plt.show()
+    def score(self, x, y):
+        y_pred = self.predict(x) # 예측값
+        y_mean = np.mean(y) # 타겟 데이터 평균
+        ss_res = np.sum((y - y_pred)**2) # residual sum of squares
+        ss_tot = np.sum((y - y_mean)**2) # total sum of squares
+        r2_score = 1 - (ss_res / ss_tot) # R^2 score
+        return r2_score
 
 # x, y 데이터를 넘파이 배열로 선언
 x_data = np.array([[1], [2], [3], [4]])
@@ -76,3 +66,11 @@ slr = SimpleLinearRegression()
 slr.fit(x_data, y_data)
 print(slr.coef_, slr.intercept_)
 print(slr.predict([[5]]))
+print(slr.score(x_data, y_data))
+
+# 산점도 그래프를 통해 x, y 데이터를 시각화
+plt.scatter(x_data, y_data)
+
+# 선그래프로 기울기와 절편을 시각화
+plt.plot(x_data, slr.coef_ * x_data + slr.intercept_, color='black', linestyle="--")
+plt.show()
