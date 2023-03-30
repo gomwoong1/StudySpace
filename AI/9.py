b3e609a4d06a6a42cfe4a6e564b12db2c5379e61
gomwoong1 <gomwoong0619@naver.com>
2023-03-30 15:21:16 +0900

    0330
    AI 로지스틱

--- AI/9.py
+++ AI/9.py
@@ -1,10 +1,27 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as mlt
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+
 
 fish = pd.read_csv("AI\data\Fish.csv")
-print(fish.head())
+# print(fish.head())
 
 fish_input = fish[['Weight', 'Length', 'Diagonal', 'Height', 'Width']].to_numpy()
 fish_target = fish[['Species']].to_numpy()
 
+train_input, test_input, train_target, test_target = train_test_split(
+    fish_input, fish_target, random_state=42)
+
+ss = StandardScaler()
+ss.fit(train_input)
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+
+kn = KNeighborsClassifier(n_neighbors=3)
+kn.fit(train_scaled, train_target)
+
+print(kn.score(train_scaled, train_target))
+print(kn.score(test_scaled, test_target))
