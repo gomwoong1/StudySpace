15a495bba5c3cdc002f1639c2f47585a741b1eed
gomwoong1 <gomwoong0619@naver.com>
2023-03-31 01:26:34 +0900

    0331
    AI Rep2 try

--- AI/report/rep_2.py
+++ AI/report/rep_2.py
@@ -5,35 +5,36 @@ class SimpleLogisticRegression():
 
     coef_, intercept_, y_pred = None, None, None
     z = None
+    
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
 
     def fit(self, x_data, y_data):
         a = 0.0
         b = 0.0
+        a_diff = 0
+        b_diff = 0
         lr = 0.003
         epochs = 2000
         n = len(x_data)
 
         for i in range(epochs + 1):
-            y_pred = a * x_data + b
+
+            for x, y in zip(x_data, y_data):
+                a_diff = x * (self.sigmoid(a * x + b) - y)
+                b_diff = self.sigmoid(a * x + b) - y
+
+                
+                a = a - lr * a_diff
+                b = b - lr * b_diff
+                
+                if i % 100 == 0:
+                    print("epoch=%d, 기울기=%0.4f, 절편=%0.4f" % (i, a, b))
             
-            a_diff = 0
-            b_diff = 0
+            # z = a * x_data + b 
+            # self.y_pred = self.sigmoid(z)
 
-            for j in range(n):
-                a_diff += x_data[j] * (x_data[j] * a + b - y_data[j])
-                b_diff += (x_data[j] * a + b - y_data[j])
-
-            a = a - lr * a_diff
-            b = b - lr * b_diff
-
-            if i % 100 == 0:
-                print("epoch=%d, 기울기=%0.4f, 절편=%0.4f" % (i, a, b))
-
-        z = a*x_data+b
-        self.y_pred = 1 / (1 + np.exp(-z))
-        # self.coef_ = a
-        # self.intercept_ = float(np.round(b,4))
-        # self.y_pred = a * x_data + b
+                
 
 x_data = np.array([[2], [4], [6], [8], [10], [12], [14]])
 y_data = np.array([0, 0, 0, 1, 1, 1, 1])
@@ -44,8 +45,3 @@ slr.fit(x_data, y_data)
 plt.scatter(x_data, y_data)
 plt.plot(x_data, slr.y_pred)
 plt.show()
-
-# z = a*x_data+b
-# # y = 1 / (1 + exp(-z))
-# plt.scatter(x_data, y_data)
-# plt.show()
