7ae323b03b6202dfd3eeb66fc080833f347c29fe
gomwoong1 <gomwoong0619@naver.com>
2023-04-02 04:00:26 +0900

    ChatCompletion API front

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,22 +1,23 @@
+// document.querySelector('#send').addEventListener('click', function () {
+//     let template = `<div class="line">
+//           <span class=" mine"> ${document.querySelector('#input').value}</span>
+//         </div>`
+//
+//     // document.getElementById('input').innerText = ''
+//     document.querySelector('.chat-content').insertAdjacentHTML('beforeend', template)
+// })
+
 // HTML 요소 찾기
 const chatMessages = document.querySelector(".chat-messages");
 const chatForm = document.querySelector(".chat-input form");
 const chatInput = document.querySelector(".chat-input input[type='text']");
 
-function addSystemChatMessage(message) {
+// 채팅 메시지 추가 함수
+function addChatMessage(message) {
     const messageEl = document.createElement("li");
     messageEl.classList.add("message");
     messageEl.textContent = message;
     chatMessages.appendChild(messageEl);
-}
-
-function addChatMessage(message) {
-    const messageEl = document.createElement("li");
-    messageEl.classList.add("message-right");
-    messageEl.textContent = message;
-
-    chatMessages.appendChild(messageEl);
-    chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
 // submit 이벤트 핸들러
