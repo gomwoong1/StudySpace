b2b0a0d1aa7532e92dd9d4cbb37476e9045d7dbb
gomwoong1 <gomwoong0619@naver.com>
2023-04-13 18:18:19 +0900

    0413
    플라스크 API 생성

--- BD/0413.py
+++ BD/0413.py
@@ -1,2 +1,16 @@
 from flask import Flask
 
+app = Flask("HelloFlask")
+
+# / URL에 대해서 RESTful API 생성
+@app.route("/")
+def home():
+    return "Hello Flask!!!"
+
+# /search URL에 대해서 RESTful API 생성
+@app.route("/search")
+def search():
+    return "Search!!!"
+
+# 서버 구동?
+app.run("0.0.0.0")
