2c5993b0cb4eadc59910895f33c4267a321908c7
gomwoong1 <gomwoong0619@naver.com>
2023-04-26 09:45:59 +0900

    0426
    데이터 확보를 위한 웹 크롤링

--- study/data_scrap.py
+++ study/data_scrap.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+res = requests.get("https://www.univ100.kr/qna/344")
+soup = BeautifulSoup(res.content, 'html.parser')
+
+# val = soup.find("div", class_="school")
+# print(val)
+print(soup)
