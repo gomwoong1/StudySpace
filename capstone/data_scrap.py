4451c834feba8e801260ae65818428afe645712b
gomwoong1 <gomwoong0619@naver.com>
2023-04-26 10:57:40 +0900

    0426
    게시글 하나 가져오기 성공

--- capstone/data_scrap.py
+++ capstone/data_scrap.py
@@ -3,6 +3,7 @@
 # from bs4 import BeautifulSoup
 
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
 import time
 
 # 시스템에 부착된 장치가 작동하지 않습니다. (0x1F) 에러 제어용 코드
@@ -11,9 +12,16 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(options=options)
 
+# 페이지 로딩을 위한 5초 대기
+driver.implicitly_wait(5)
+
+# URL에 접근
 driver.get('https://www.univ100.kr/qna/344')
 
-# 페이지 로딩을 위한 5초 대기
+
+item = driver.find_element(By.CLASS_NAME, "qna")
+# item.click()
+
 time.sleep(5)
 
 # driver.close()
