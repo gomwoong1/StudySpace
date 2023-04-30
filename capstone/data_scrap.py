06a4d0b70636bbdbdac6238a86d7c24b7e82134c
gomwoong1 <gomwoong0619@naver.com>
2023-04-30 22:06:37 +0900

    0430
    - 코드정리
    - 데이터 중복 디버깅

--- capstone/data_scrap.py
+++ capstone/data_scrap.py
@@ -86,10 +86,10 @@ while True:
     # 답변
     answers = driver.find_elements(By.CSS_SELECTOR, "div[class='answer__text']")
 
-    print("\nnum: {}, title: {}\n".format(i, title))
-    print("카테고리: {}, 학과: {}, 날짜: {}".format(category, dept, date))
-    print(content)
-    print("-"*100)
+    # print("\nnum: {}, title: {}\n".format(i, title))
+    # print("카테고리: {}, 학과: {}, 날짜: {}".format(category, dept, date))
+    # print(content)
+    # print("-"*100)
 
     for answer in answers:
         try:
@@ -106,28 +106,8 @@ while True:
     driver.back()
     time.sleep(0.5)
     i += 1
-
-# 안 쓰지만 백업용
-# while True:
-# 	#현재 스크롤의 가장 아래로 내림
-#     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     
-#     #전체 스크롤이 늘어날 때까지 대기
-#     time.sleep(1)
-#     # driver.implicitly_wait(1)
-    
-#     #늘어난 스크롤 높이
-#     scroll_height = driver.execute_script("return document.body.scrollHeight")
-
-#     #늘어난 스크롤 위치와 이동 전 위치 같으면(더 이상 스크롤이 늘어나지 않으면) 종료
-#     if scroll_location == scroll_height:
-#         break
-	
-#     #같지 않으면 스크롤 위치 값을 수정하여 같아질 때까지 반복
-#     else:
-#         #스크롤 위치값을 수정
-#         scroll_location = driver.execute_script("return document.body.scrollHeight")
-
+# 브라우저 종료 방지용 delay
 time.sleep(180)
 
 driver.close()
