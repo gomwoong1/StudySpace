346feb1fa3742e342bbd6c0fdc776ef091c9e79b
gomwoong1 <gomwoong0619@naver.com>
2023-04-30 22:13:56 +0900

    0430
    - 답변이 없을 경우 기본 답변 만들어서 붙여주기
    - 답변이 있다면 for문 사용해서 답변 붙여주기

--- capstone/data_scrap.py
+++ capstone/data_scrap.py
@@ -86,22 +86,23 @@ while True:
     # 답변
     answers = driver.find_elements(By.CSS_SELECTOR, "div[class='answer__text']")
 
-    # print("\nnum: {}, title: {}\n".format(i, title))
-    # print("카테고리: {}, 학과: {}, 날짜: {}".format(category, dept, date))
-    # print(content)
-    # print("-"*100)
 
-    for answer in answers:
-        try:
-            ans = answer.find_element(By.CSS_SELECTOR, "div[class='answer__text']").text
-            print(ans)
-            print("-"*100)
-        except:
-            print("\nnum: {}, title: {}\n".format(i, title))
-            print("카테고리: {}, 학과: {}, 날짜: {}".format(category, dept, date))
-            print(content)
-            print("-"*100)
-            print("")
+    if len(answers) == 0:
+        ans = "답변이 없습니다."
+    
+    else:
+        # 답변 개수 별 답변 매핑
+        for answer in answers:
+            try:
+                ans = answer.find_element(By.CSS_SELECTOR, "div[class='answer__text']").text
+                print(ans)
+                print("-"*100)
+            except:
+                print("\nnum: {}, title: {}\n".format(i, title))
+                print("카테고리: {}, 학과: {}, 날짜: {}".format(category, dept, date))
+                print(content)
+                print("-"*100)
+                print("")
     
     driver.back()
     time.sleep(0.5)
