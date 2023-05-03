32364412c25137d1402f6ce78f863bd83083cae7
gomwoong1 <gomwoong0619@naver.com>
2023-05-03 15:02:10 +0900

    0503
    flask 연습

--- capstone/flask/app.py
+++ capstone/flask/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask("TestFlask")
+# templates 폴더내에 html 파일이 있음을 명시
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
     return "Flask Test!"
 
-app.run("0.0.0.0")
+@app.route("/test")
+def test():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run("0.0.0.0")
