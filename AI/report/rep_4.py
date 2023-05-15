1f5479daa0927904237fdf284d636538bc534739
gomwoong1 <gomwoong0619@naver.com>
2023-05-16 01:49:04 +0900

    0516 csv 필터링
    각 해 1/1일의 전국 데이터만 남긴 csv 파일로 핸들링하는 코드 작성

--- AI/report/rep_4.py
+++ AI/report/rep_4.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 df = pd.read_csv(r"C:\Users\gomwo\Desktop\git\python_practice\AI\report\Korean_demographics_2000-2022.csv")
 
-filtered_df = df[df['Region'] == 'Whole country']
-print(filtered_df)
+filtered_df = df[(df['Date'].str.startswith('1/1/')) & (df['Region'] == 'Whole country')]
+
+filtered_df.to_csv('whole_country.csv', index=False)
