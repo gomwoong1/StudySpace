f4fc2fd7b76989221ce0ee1d7093eba7b6bc7311
gomwoong1 <gomwoong0619@naver.com>
2023-05-16 11:08:14 +0900

    0516 65세 이상 연령 예측 회귀모델
    2000~2022년 65세 이상 인구의 데이터를 통해 선형회귀 모델로 인구 예측하는 모델 생성

--- AI/report/rep_4.py
+++ AI/report/rep_4.py
@@ -2,17 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv(r"C:\Users\gomwo\Desktop\git\python_practice\AI\report\2039.csv")
+VALUE = 2035
 
-seoul = df[df['region'] == 'seoul']
-seoul_total_values = np.array(seoul['total'])
+df = pd.read_csv(r"C:\Users\gomwo\Desktop\git\python_practice\AI\report\data\2039.csv")
+df2 = pd.read_csv(r"C:\Users\gomwo\Desktop\git\python_practice\AI\report\data\65.csv")
+
+tmp = df[df['region'] == 'seoul']
+seoul_total = np.array(tmp['total'])
+
+tmp = df2[df2['region'] == 'seoul']
+seoul_total2 = np.array(tmp['total'])
 
 years = np.arange(2000, 2022 + 1)
 years = years.reshape(-1,1)
 
-print(seoul_total_values)
+print(seoul_total)
+print(seoul_total2)
 
 lr = LinearRegression()
+lr2 = LinearRegression()
 
-lr.fit(years, seoul_total_values)
-print(lr.predict([[2039]]))
+lr.fit(years, seoul_total)
+val_2039 = lr.predict([[VALUE]])
+print(val_2039)
+
+lr2.fit(years, seoul_total2)
+val_65 = lr2.predict([[VALUE]])
+print(val_65)
+
+print("{}년 서울지역 소멸위험지수: {}%".format(VALUE, val_2039/val_65))
