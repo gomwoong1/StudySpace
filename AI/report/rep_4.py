f68e9d06c462aaf1b1f7ccb6720e83b792b87328
gomwoong1 <gomwoong0619@naver.com>
2023-05-16 11:39:24 +0900

    0516
    전반적인 모델 작성 및 로직 구현 완료

--- AI/report/rep_4.py
+++ AI/report/rep_4.py
@@ -4,30 +4,34 @@ from sklearn.linear_model import LinearRegression
 
 VALUE = 2035
 
+region = ["seoul", "busan", "daegu", "incheon", "gwangju", "daejeon", "ulsan", "gyeonggi", "gangwon", 
+        "chungcheongbukdo", "chungcheongnamdo", "jeonnabukdo", "jeonnanamdo", "gyeoungsangbukdo", 
+        "gyeoungsangnamdo", "jeju"]
+
+region_kor = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "경기도", "강원도", "충청북도",
+            "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주"]
+
+years = np.arange(2000, 2023)
+years = years.reshape(-1,1)
+
 df = pd.read_csv(r"C:\Users\gomwo\Desktop\git\python_practice\AI\report\data\2039.csv")
 df2 = pd.read_csv(r"C:\Users\gomwo\Desktop\git\python_practice\AI\report\data\65.csv")
 
-tmp = df[df['region'] == 'seoul']
-seoul_total = np.array(tmp['total'])
+female_lr = LinearRegression()
+old_lr = LinearRegression()
 
-tmp = df2[df2['region'] == 'seoul']
-seoul_total2 = np.array(tmp['total'])
+result = {}
 
-years = np.arange(2000, 2022 + 1)
-years = years.reshape(-1,1)
+for reg,reg_kor in zip(region, region_kor):
+    tmp = df[df['region'] == reg]
+    female = np.array(tmp['total'])
 
-print(seoul_total)
-print(seoul_total2)
+    tmp = df2[df2['region'] == reg]
+    old = np.array(tmp['total'])
 
-lr = LinearRegression()
-lr2 = LinearRegression()
+    female_lr.fit(years, female)
+    old_lr.fit(years, old)
 
-lr.fit(years, seoul_total)
-val_2039 = lr.predict([[VALUE]])
-print(val_2039)
+    result[reg_kor] = round(float(female_lr.predict([[VALUE]]) / old_lr.predict([[VALUE]])),2)
 
-lr2.fit(years, seoul_total2)
-val_65 = lr2.predict([[VALUE]])
-print(val_65)
-
-print("{}년 서울지역 소멸위험지수: {}%".format(VALUE, val_2039/val_65))
+print("{}년 전국지역 소멸위험지수: {}%".format(VALUE, result))
